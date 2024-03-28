142a64e48fb20a5d0948944cc30390e0efeb5b44
Sourav Saha <sourav.saha@codeclouds.com>
2024-03-28 14:20:33 +0530

    Complete tutorial

--- src/App.jsx
+++ src/App.jsx
@@ -1,149 +1,119 @@
-// import './App.css'
-import { useEffect, useState } from "react";
-import Inputgroup from "./components/form/Inputgroup";
+import React from "react";
+import useForm from "./hooks/useForm";
+import InputGroup from "./components/form/Inputgroup";
 import Button from "./components/ui/buttons/Button";
-import { deepClone } from "./utils/deep-clone";
 
 const init = {
-  title: { values: "", errors: "", focus: false },
-  bio: { values: "", errors: "", focus: false },
-  skills: { values: "", errors: "", focus: false },
+  firstName: "Sourav Saha",
+  lastName: "",
+  email: "",
+  password: "",
 };
 
-function App() {
-  const [state, setState] = useState({ ...init });
+const validate = (values) => {
+  const errors = {};
+  if (!values.firstName) {
+    errors["firstName"] = "First name is required";
+  }
+  if (!values.lastName) {
+    errors["lastName"] = "last name is required";
+  }
+  if (!values.email) {
+    errors["email"] = "Email is required";
+  }
+  if (!values.password) {
+    errors["password"] = "Password is required";
+  }
 
-  const mapStateToValue = (state) => {
-    return Object.keys(state).reduce((acc, cur) => {
-      acc[cur] = state[cur].values;
-      return acc;
-    }, {});
-  };
+  return errors;
+};
 
-  const checkValidity = (values) => {
-    const errors = {};
-    const { title, bio, skills } = values;
-    if (!title) {
-      errors["title"] = "Invalid title";
-    }
-    if (!bio) {
-      errors["bio"] = "Invalid bio";
-    }
-    if (!skills) {
-      errors["skills"] = "Invalid skills";
-    }
-    return {
-      errors,
-      isValid: Object.keys(errors).length === 0,
-    };
-  };
+export default function App() {
+  const {
+    formState: state,
+    handleBlur,
+    handleFocus,
+    handleChange,
+    handleSubmit,
+    clear,
+  } = useForm({ init, validate });
 
-  const handleChange = (e) => {
-    const { name: key, value } = e.target;
-
-    const oldState = deepClone(state);
-    oldState[key].values = value;
-
-    const values = mapStateToValue(oldState);
-    const { errors } = checkValidity(values);
-
-    if (oldState[key].focus && errors[key]) {
-      oldState[key].errors = errors[key];
+  // console.log(state.firstName.value);
+  const submit = ({ hasErrors, values, errors }) => {
+    if (hasErrors) {
+      alert("[ERROR]" + JSON.stringify(errors,null, 2));
     } else {
-      oldState[key].errors = "";
+      alert("[SUCCESS]" + JSON.stringify(values));
     }
-    // console.log("oldState: ", oldState);
-    setState(oldState);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const values = mapStateToValue(state);
-
-    const { isValid, errors } = checkValidity(values);
-
-    if (isValid) {
-      console.log("validated", state);
-    } else {
-      const oldState = deepClone(state);
-      Object.keys(errors).forEach((key) => {
-        oldState[key].errors = errors[key];
-      });
-      setState(oldState);
-    }
-  };
-
-  const handleFocus = (e) => {
-    const { name } = e.target;
-    const oldState = deepClone(state);
-    oldState[name].focus = true;
-    setState(oldState);
-  };
-
-  const handleBlur = (e) => {
-    const key = e.target.name;
-    const values = mapStateToValue(state);
-    const { errors } = checkValidity(values);
-    const oldState = deepClone(state);
-    if (oldState[key].focus && errors[key]) {
-      oldState[key].errors = errors[key];
-    } else {
-      oldState[key].errors = "";
-    }
-    setState(oldState);
   };
 
   return (
-    <>
-      <div className="root">
-        <form onSubmit={handleSubmit}>
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              gap: "1rem",
-              width: "40%",
-              margin: "10% auto",
-            }}
+    <div className="root">
+      <form onSubmit={(e) => handleSubmit(e, submit)}>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "1rem",
+            width: "40%",
+            margin: "5% auto",
+          }}
+        >
+          <InputGroup
+            label={"First Name"}
+            name="firstName"
+            placeholder={"John"}
+            value={state.firstName.value}
+            onChange={handleChange}
+            error={state.firstName.errors}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+          />
+          <InputGroup
+            label={"Last Name"}
+            name="lastName"
+            placeholder={"Doe"}
+            value={state.lastName.value}
+            onChange={handleChange}
+            error={state.lastName.errors}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+          />
+
+          <InputGroup
+            label={"Email"}
+            name="email"
+            placeholder={"johndoe@example.com"}
+            value={state.email.value}
+            onChange={handleChange}
+            error={state.email.errors}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+          />
+
+          <InputGroup
+            label={"Password"}
+            name="password"
+            placeholder={"********"}
+            value={state.password.value}
+            onChange={handleChange}
+            error={state.password.errors}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+          />
+
+          <Button type="reset" onClick={clear} style={{ padding: "15px 0px" }}>
+            Clear
+          </Button>
+
+          <Button
+            type="submit"
+            style={{ padding: "15px 0px", backgroundColor: "green" }}
           >
-            <Inputgroup
-              label={"Title"}
-              name="title"
-              placeholder={"Software Engineer"}
-              value={state.title.values}
-              onChange={handleChange}
-              error={state.title.errors}
-              onFocus={handleFocus}
-              onBlur={handleBlur}
-            />{" "}
-            <Inputgroup
-              label={"Bio"}
-              name="bio"
-              placeholder={"I am software engineer..."}
-              value={state.bio.values}
-              onChange={handleChange}
-              error={state.bio.errors}
-              onFocus={handleFocus}
-              onBlur={handleBlur}
-            />{" "}
-            <Inputgroup
-              label={"Skills"}
-              name="skills"
-              placeholder={"javascript, react"}
-              value={state.skills.values}
-              onChange={handleChange}
-              error={state.skills.errors}
-              onFocus={handleFocus}
-              onBlur={handleBlur}
-            />
-            <Button type="submit" style={{ padding: "14px 0px" }}>
-              Submit
-            </Button>
-          </div>
-        </form>
-      </div>
-    </>
+            Submit
+          </Button>
+        </div>
+      </form>
+    </div>
   );
 }
-
-export default App;
--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,5 +1,131 @@
-const useForm = () => {
+import { useState } from "react";
+import { deepClone, isObjEmpty } from "../utils/deep-clone";
 
+/**
+ * @typedef {Object} Param
+ * @property {Object} init
+ * @property {(Object | boolean)} validate
+ * create form using this useForm hook easily
+ * @param {Param} param0
+ * @returns
+ */
+
+const useForm = ({ init, validate }) => {
+  const [state, setState] = useState(() => mapValuesToState(init));
+
+  
+  const handleChange = (e) => {
+    const { name: key, value } = e.target;
+
+    const oldState = deepClone(state);
+    oldState[key].value = value;
+
+    const { errors } = getErrors();
+
+    if (oldState[key].touched && errors[key]) {
+      oldState[key].errors = errors[key];
+    } else {
+      oldState[key].errors = "";
+    }
+    setState({...oldState});
+  };
+
+
+  const handleFocus = (e) => {
+    const { name } = e.target;
+    const oldState = deepClone(state);
+    oldState[name].focused = true;
+
+    if (!oldState[name].touched) {
+      oldState[name].touched = true;
+    }
+
+    setState(oldState);
+  };
+
+  const handleBlur = (e) => {
+    const key = e.target.name;
+    const { errors } = getErrors();
+    const oldState = deepClone(state);
+    if (oldState[key].touched && errors[key]) {
+      oldState[key].errors = errors[key];
+    } else {
+      oldState[key].errors = "";
+    }
+    oldState[key].focused = false;
+    setState(oldState);
+  };
+
+  const handleSubmit = (e, cb) => {
+    e.preventDefault();
+
+    const { hasErrors, values, errors } = getErrors();
+    cb({
+      hasErrors,
+      values,
+      errors,
+      touched: mapStateToKeys(state, "touched"),
+      focused: mapStateToKeys(state, "focused"),
+    });
+  };
+
+  const clear = () => {
+    const newState = mapValuesToState(init, true);
+    setState({ ...newState });
+  };
+
+  const getErrors = () => {
+    let hasErrors = null,
+      errors = null;
+    const values = mapStateToKeys(state, "value");
+
+    if (typeof validate === "boolean") {
+      hasErrors = validate;
+      errors = mapStateToKeys(state, "errors");
+    } else if (typeof validate === "function") {
+      const errorsFromCb = validate(values);
+      hasErrors = !isObjEmpty(errorsFromCb);
+      errors = errorsFromCb;
+    } else {
+      throw new Error("Validate property must be boolean or function");
+    }
+
+    return {
+      values,
+      errors,
+      hasErrors,
+    };
+  };
+
+  return {
+    formState: state,
+    handleChange,
+    handleFocus,
+    handleBlur,
+    handleSubmit,
+    clear
+  };
 };
 
 export default useForm;
+
+// helper functions
+
+const mapValuesToState = (values, shouldClear = false) => {
+  return Object.keys(values).reduce((acc, key) => {
+    acc[key] = {
+      value: shouldClear ? "" : values[key],
+      error: "",
+      focused: false,
+      touched: false,
+    };
+    return acc;
+  }, {});
+};
+
+const mapStateToKeys = (state, key) => {
+  return Object.keys(state).reduce((acc, cur) => {
+    acc[cur] = state[cur][key];
+    return acc;
+  }, {});
+};
